686f9a4251e08900018df23092cedee6a614fa50
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2025-01-31 00:36:50 -0300

    feat: Adding custom enum for Images

---
+++ Oficial_SSC2025.swiftpm/Constants/ImageConstants.swift
@@ -0,0 +1,14 @@
+//
+//  ImageConstants.swift
+//  Oficial_SSC2025
+//
+//  Created by Leonardo Mesquita Alves on 31/01/25.
+//
+
+enum ImageConstants {
+    //Button Images
+    static let nextButton = "next_button"
+    static let restartButton = "restart_button"
+    
+    //
+}
